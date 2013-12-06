3094975cab6a243d56715314d278bef34a2ea6a7
Chris Berger <contact@christopherberger.info>
2013-12-05 22:31:46 -0800

    cleaned up commit errors

--- Bluefinder/src/main/java/com/digitalobstaclecourse/bluefinder/FindCar.java
+++ Bluefinder/src/main/java/com/digitalobstaclecourse/bluefinder/FindCar.java
@@ -30,32 +30,20 @@
 package com.digitalobstaclecourse.bluefinder;
 
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothClass;
 import android.bluetooth.BluetoothDevice;
-import android.content.ComponentName;
-import android.content.Context;
+
 import android.content.Intent;
-import android.content.ServiceConnection;
-import android.content.SharedPreferences;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.IBinder;
-import android.os.RemoteException;
-import android.preference.PreferenceFragment;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ListView;
+
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 
-import com.android.vending.billing.IInAppBillingService;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Set;
@@ -63,102 +51,10 @@ import java.util.Set;
 public class FindCar extends FragmentActivity implements
         NotifyNoBluetoothDialog.NoticeDialogListener, BluetoothDeviceListFragment.Callbacks {
     private static String TAG = "FindCar";
-    private static final String PREFS_NAME = "LOCATION_KEY_VALUE";
     private BluetoothAdapter mBluetoothAdapter;
-    private ListView mListView;
-
-    // private ArrayAdapter<String> mListAdapter;
-    private BluetoothDeviceAdapter mListAdapter;
     private boolean mTwoPane = false;
     private ArrayList<BluetoothDeviceInfo> device_info_list;
-    private LocationManager locationManager;
-    private String ITEM_TYPE_INFINITE = "bluefinder_full_pass";
-    IInAppBillingService _service;
-
-
-    private ServiceConnection mServiceConn;
-
-
-    public void startSetup(final OnIabSetupFinishedListener listener) {
-        mServiceConn =             new ServiceConnection() {
-            @Override
-            public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
-                _service = IInAppBillingService.Stub.asInterface(iBinder);
-                Log.d(TAG, "onServiceConnected");
-            }
-
-            @Override
-            public void onServiceDisconnected(ComponentName componentName) {
-                _service = null;
-            }
-        };
-
-    }
-
-    Bundle get_available_purchases() throws RemoteException {
-       ArrayList skuList = new ArrayList();
-        skuList.add(ITEM_TYPE_INFINITE);
-        Bundle querySkus = new Bundle();
-        querySkus.putStringArrayList("ITEM_ID_LIST", skuList);
-        if (_service == null) {
-            Log.e(TAG, "service is null");
-        }
-
-        Bundle skuDetails = _service.getSkuDetails(3,getPackageName(),"inapp",querySkus);
-        return skuDetails;
-
-
-    }
-
-    void log_purchases() {
-        try {
-            Bundle purchases = get_available_purchases();
-            int response = purchases.getInt("RESPONSE_CODE");
-            if (response == 0) {
-                ArrayList<String> purchases_details = purchases.getStringArrayList("DETAILS_LIST");
-                for (String thisResponse : purchases_details) {
-                    JSONObject object = new JSONObject(thisResponse);
-                    String sku = object.getString("productId");
-                    String price = object.getString("price");
-                    Log.d(TAG, String.format("productID = %s, %s", sku, price));
-
-
-                }
-            }
-
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-    void handle_items() {
-        mServiceConn = new ServiceConnection() {
-            @Override
-            public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
-                IInAppBillingService _service = IInAppBillingService.Stub.asInterface(iBinder);
-
-
-                try {
-                    Bundle available_purchases =_service.getSkuDetails(3,getPackageName(),"inapp", null);
-                    Bundle owned_items = _service.getPurchases(3,getPackageName(),ITEM_TYPE_INFINITE, null);
-                    int response = owned_items.getInt("RESPONSE_CODE");
-                    if (response == 0) {
-                        ArrayList ownedSkus = owned_items.getStringArrayList("INAPP_PURCHASE_ITEM_LIST");
-                    }
-
-                } catch (RemoteException e) {
-                    e.printStackTrace();
-                }
-
-            }
-
-            @Override
-            public void onServiceDisconnected(ComponentName componentName) {
-
-            }
-        };
-    }
+    //private static String ITEM_TYPE_INFINITE = "bluefinder_full_pass";
 
 
     void insert_paired_devices_into_database() {
@@ -170,9 +66,14 @@ public class FindCar extends FragmentActivity implements
         Set<BluetoothDevice> paired_devices = mBluetoothAdapter
                 .getBondedDevices();
         DataAccessModule dataAccess = DataAccessModule.getDataAccessModule(this);
-        for (BluetoothDevice d : paired_devices) {
-            Log.d(TAG, "msg" + d.getName());
-            dataAccess.add_device(d);
+        if (paired_devices != null) {
+            for (BluetoothDevice d : paired_devices) {
+                Log.d(TAG, "msg" + d.getName());
+                dataAccess.add_device(d);
+            }
+        }
+        else {
+            Log.e(TAG, "paired_devices  == null");
         }
         Log.d(TAG, "Logging all devices");
         for (BluetoothDeviceInfo info : dataAccess.getAllDevices()) {
@@ -189,7 +90,7 @@ public class FindCar extends FragmentActivity implements
         Log.d("onCreate", "play services available? " +
                 (ConnectionResult.SUCCESS == GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext())));
         
-        bindService(new Intent("com.android.vending.billing.InAppBillingService.BIND"), mServiceConn, Context.BIND_AUTO_CREATE);
+        //bindService(new Intent("com.android.vending.billing.InAppBillingService.BIND"), mServiceConn, Context.BIND_AUTO_CREATE);
         Log.d(TAG,"service Bound");
         //log_purchases();
 
@@ -205,15 +106,12 @@ public class FindCar extends FragmentActivity implements
         }
 
 
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
         DataAccessModule dataAccess = DataAccessModule.getDataAccessModule(this);
         dataAccess.verify_db_existence(DataAccessModule.SQLModelOpener.DEVICE_TABLE_NAME);
         dataAccess.verify_db_existence(DataAccessModule.SQLModelOpener.LOCATION_TABLE_NAME);
-        BluetoothDeviceInfo[] devices = dataAccess.getAllDevices();
 
-        final SharedPreferences locationKeyValue = getSharedPreferences(
-                PREFS_NAME, 0);
+
+
         ArrayList<String> device_name_list = new ArrayList<String>();
         device_info_list = new ArrayList<BluetoothDeviceInfo>();
         insert_paired_devices_into_database();
@@ -234,13 +132,7 @@ public class FindCar extends FragmentActivity implements
 
 
     }
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (mServiceConn != null) {
-            unbindService(mServiceConn);
-        }
-    }
+
 
     public void onItemSelected(String id) {
         Log.d(TAG, "Item selected: " + id);
@@ -258,38 +150,18 @@ public class FindCar extends FragmentActivity implements
         }
     }
 
-    protected void makeUseOfNewLocation(String location, int id) {
-        Log.d("LOCATION", "location = " + location.toString());
-        Intent intent = new Intent();
-        //Parcel p = new Parcel();
-        //intent.putExtra(location.writeToParcel(parcel, flags), value)
-        intent.putExtra("DEVICE_ID", id);
-        intent.putExtra("LAST_GPS_LOCATION", location);
-
-        //intent.putExtra("LAST_GPS_LOCATION", location.writeToParcel(parcel, 0));
-        intent.setClass(this, FindCarLocatorActivity.class);
-        startActivity(intent);
-
-    }
-
-    public static class PrefsFragment extends PreferenceFragment {
-
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            getActivity();
-
-            // Load the preferences from an XML resource
-            addPreferencesFromResource(R.xml.preferences);
-        }
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_find_car, menu);
         MenuItem _menuItemAction = menu.findItem(R.id.menu_ammo);
-        View actionView =_menuItemAction.getActionView();
+        View actionView = null;
+        if (_menuItemAction != null) {
+            actionView = _menuItemAction.getActionView();
+        }
+        else {
+            Log.e(TAG, "getActionView()");
+        }
         assert actionView != null;
         actionView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -323,6 +195,4 @@ public class FindCar extends FragmentActivity implements
         finish();
     }
 
-    private class OnIabSetupFinishedListener {
-    }
 }
